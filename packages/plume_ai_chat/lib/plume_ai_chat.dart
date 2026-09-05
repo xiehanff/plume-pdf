b50747210e419ff089aa29645f31ccd54f4ccc7d
hax <xiehanff@gmail.com>
2026-09-06 03:21:03 +0800

    refactor: export conversation presenter

--- packages/plume_ai_chat/lib/plume_ai_chat.dart
+++ packages/plume_ai_chat/lib/plume_ai_chat.dart
@@ -3,6 +3,7 @@ library plume_ai_chat;
 export 'src/backend/ai_backend.dart';
 export 'src/backend/deepseek_backend.dart';
 export 'src/controller/ai_chat_controller.dart';
+export 'src/controller/ai_conversation_presenter.dart';
 export 'src/core/ai_chat_session.dart';
 export 'src/core/ai_response_parser.dart';
 export 'src/models/ai_chat_history_message.dart';
