3cb4957573cd38c13895faeb363963f6cfced084
hax <xiehanff@gmail.com>
2026-09-06 07:50:18 +0800

    test: import PDF AI streaming controller directly

--- test/streaming_ai_sidebar_controller_test.dart
+++ test/streaming_ai_sidebar_controller_test.dart
@@ -6,8 +6,8 @@ import 'package:flutter/rendering.dart' show ScrollDirection;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:plume_ai_chat/plume_ai_chat.dart';
-import 'package:plume_pdf/app/modules/home/controllers/ai_sidebar_controller.dart';
-import 'package:plume_pdf/app/modules/home/models/pdf_ai_panel_state.dart';
+import 'package:plume_pdf/app/modules/pdf_ai/controllers/ai_sidebar_controller.dart';
+import 'package:plume_pdf/app/modules/pdf_ai/models/pdf_ai_panel_state.dart';
 import 'package:plume_pdf/app/modules/home/views/widgets/ai_sidebar.dart';
 
 class _ControlledBackend implements AiBackend {
