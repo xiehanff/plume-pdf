94249036b247ec18494e4ff70992238ed31f1ea2
hax <xiehanff@gmail.com>
2026-09-06 04:21:52 +0800

    refactor(home): re-export reader outline model

--- lib/app/modules/home/models/pdf_outline_entry.dart
+++ lib/app/modules/home/models/pdf_outline_entry.dart
@@ -1,13 +1,1 @@
-class PdfOutlineEntry {
-  const PdfOutlineEntry({
-    required this.id,
-    required this.title,
-    required this.pageNumber,
-    required this.depth,
-  });
-
-  final String id;
-  final String title;
-  final int pageNumber;
-  final int depth;
-}
+export '../../reader/models/pdf_outline_entry.dart';
